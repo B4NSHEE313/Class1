0d96e7d9d7a620881af60f72f9fbc82cc0008815
David <Sidewalksleepin@gmail.com>
2025-04-09 18:48:23 -0400

    Save my changes

--- Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
+++ Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
@@ -5,42 +5,80 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        // Define the variables to hold the numbers we need to add
-        int number1 = 0; // Since we will be doing math it needs to be numeric
-        int number2 = 0; // Since we will be doing math it needs to be numeric
-        int number3 = 0; // Since we will be doing math it needs to be numeric
+ static void Main(string[] args)
+ {
+  // Define an array to hold 5 numbers
+  int[] numbers = new int[5]; // numbers.Length =3; 2 is the largest allowable index
 
-        // Define a variable to hold the sum of the numbers
-        int sum = 0;
 
-        Console.WriteLine("--- Starting program ---");
+  // Define a variable to hold the sum of the numbers
+  int sum = 0;
 
-        // Ask for the numbers one at time using the C# Console object
-        //     which represents the keyboard and screen
-        // Console.ReadLine() returns a string from keyboard - cannot store in an int
-        Console.WriteLine("Please enter a number:  "); // Asking for the number
-        string theResponse; // Define a string to hold the line of input from the keyboard
-        theResponse = Console.ReadLine(); // Get a line from the keyboard
-      
-        // int.Parse(string) will convert a string to an int
-        number1 = int.Parse(theResponse);
+  Console.WriteLine("--- Starting program ---");
 
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number2 = int.Parse(theResponse);
+  //Normally we process an array from beginning to end
+  // (From the first element to the last element, one at a time)
+  //
+  // a for-loop will loop through a process counting as it does so
+  // a for-loop is an excellent tool for processing an array from start to end
 
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number3 = int.Parse(theResponse);
+  // standard syntax: for(i=0; i < arrayName.length; i++)
+  //
+  // arrayName.length is the number of elements in the array
 
-        // Add the numbers together create a sum/total
-        sum = number1 + number2 + number3;
-       
-        // Tell the requester the sum/total (display)
-        Console.WriteLine("The sum is: " + sum);
 
-        Console.WriteLine("--- Ending program ---");
-    }
+  //index loop as
+  //start long as the index increment the
+  // at 0 is not outside array index for each loop
+
+  int i;
+  for (i = 0; i < numbers.Length; i++) // i=0, 1, 2 inside loop and 3 when exit loop
+  {
+   Console.WriteLine("Please enter a number:  "); // Asking for the number
+   numbers[i] = int.Parse(Console.ReadLine()); // Get a line from the keyboard //convert it to an int //store it in numbers [i]
+
+
+   for (i = 0; i < numbers.Length; i++)
+   {
+    Console.WriteLine("Element #" + i +  "is: " + numbers[i]);
+    sum = sum + numbers[i]; // Add the number to the sum
+   }
+   // Ask for the numbers one at time using the C# Console object
+   //     which represents the keyboard and screen
+   // Console.ReadLine() returns a string from keyboard - cannot store in an int
+   
+
+   // int.Parse(string) will convert a string to an int
+   // number1 = int.Parse(theResponse);
+
+   // Console.WriteLine("Please enter a number:  ");
+   //  theResponse = Console.ReadLine(); // Reuse theResponse define above
+   // number2 = int.Parse(theResponse);
+
+   //  Console.WriteLine("Please enter a number:  ");
+   // theResponse = Console.ReadLine(); // Reuse theResponse define above
+   // number3 = int.Parse(theResponse);
+
+   //Console.WriteLine("Please enter a number:  ");
+   // theResponse = Console.ReadLine(); // Reuse theResponse define above
+   // number4 = int.Parse(theResponse);
+
+
+   // Console.WriteLine("Please enter a number:  ");
+   //theResponse = Console.ReadLine(); // Reuse theResponse define above
+   // number5 = int.Parse(theResponse);
+
+
+
+   // Add the numbers together create a sum/total
+   //sum = number1 + number2 + number3 + number4 + number5;
+
+   // Tell the requester the sum/total (display)
+   Console.WriteLine("The sum is: " + sum);
+
+   Console. WriteLine(" The average of the numbers is: " + (double) (sum / numbers.Length));
+   
+   Console.WriteLine("--- Ending program ---");
+  }
+ }
 }
--- Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
+++ Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
@@ -15,6 +15,9 @@ class Program
          *        you can't use < > to compare strings
          *        Beware: Early versions of C# do not allow use of == with strings
          *
+         *
+         * a method() is a program that processes data in an object/variable
+         *
          * C# provides several methods to operate on strings:
          *
          *    Hey) - compare the string to left of the . to string inside () for equals
@@ -59,5 +62,51 @@ class Program
         {
             Console.WriteLine("string1 is NOT equal string2");
         }
+        
+        // Some methods that process strings
+        // to see a list of all methods available to an object - just type the object name and a dot: ObjectName.
+        //.Length - returns the number of characters in the string
+       // . Contains(string) - returns true if the string given is inside a string ()
+        // .Substring(Start-index,Length) - extract charachters from a string starting at the start index for the numbers of characters indicated by the length
+       // The start index starts at 0 not 1.
+       // //whenever you see the word index - starts counting at 0
+       // whenever you see the position of a character - starts counting at 1
+        // .StartsWith(string) - returns true if the string starts with the specified chars/Characters (case sensitive)
+       // .EndsWith(string) - returns true if the string ends with the specified chars/Characters (case sensitive)
+       // .Trim() - removes leading and trailing spaces from a string
+       
+       //string1.
+       
+       
+       string sentence = "      My name is david and i learn C#     "; //true if sentence contains David
+        Console.WriteLine("There are"+ sentence.Length+ " characters in the sentence");
+        
+        bool contains = sentence.Contains("david");
+        Console.WriteLine("Does sentence contain david:" + contains);
+
+        string nameInSentence = sentence.Substring(17, 5);
+Console.WriteLine("Name is: " + nameInSentence);
+      Console.WriteLine("Does sentence start with My?" + sentence.StartsWith("My"));
+      Console.WriteLine("Does sentence start with my?" + sentence.StartsWith("my"));
+      Console.WriteLine("Does sentence end with C#?" + sentence.EndsWith("C#"));
+
+      Console.WriteLine("Result when sentence.Trim() is used to remove leading and trailing spaces");
+      Console.WriteLine("Does sentence start with My?" + sentence.Trim().StartsWith("My"));
+      Console.WriteLine("Does sentence start with my?" + sentence.Trim().StartsWith("my"));
+      Console.WriteLine("Does sentence end with C#?" + sentence.Trim().EndsWith("C#")); 
+       
+
+     
+      
+      // // When C# sees a statement with chained operations (operations separated by a dot)
+      // C# Process the statement from left to right, one operation at a time
+      // Uses the result of operation in the next one
+      
+      // sentence.trim().Endswith("C#")
+
+      // 1. sentence.Trim() - returns a string with leading and trailing spaces removed
+        // 2. results-from-step1 .EndsWith("C")
+      
+      
     } // End of Main()
 }  // End of class Program
