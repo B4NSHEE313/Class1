e6ddaaf396091aa93e95f11d7d86c846bb731285
David <Sidewalksleepin@gmail.com>
2025-05-01 21:33:27 -0400

    m

--- Unit-4-Intro-To-Object-Oriented-Programming/Day-4-Playing-Card-Frank/Day-4-Playing-Card-Frank/Program.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-4-Playing-Card-Frank/Day-4-Playing-Card-Frank/Program.cs
@@ -43,8 +43,8 @@ namespace Day_2_Playing_Card_Example
              *     and display them both
              *************************************************************************/
 
-         // PlayingCard newCard = aCard;  // This makes both newCard and aCard point to the same object
-            PlayingCard newCard = new PlayingCard(aCard); // Use copy constructor
+         // PlayingCard newCard = aCard;  // copy the reference - This makes both newCard and aCard point to the same object
+            PlayingCard newCard = new PlayingCard(aCard); // Use copy constructor copy the data
 
             Console.WriteLine($"  aCard is: {aCard}");
             Console.WriteLine($"newCard is: {newCard}");
