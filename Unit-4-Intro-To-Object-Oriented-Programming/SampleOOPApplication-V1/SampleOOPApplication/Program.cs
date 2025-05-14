01ef95c0f0d57417037dca026df425abe8b77a65
David <Sidewalksleepin@gmail.com>
2025-05-14 17:34:04 -0400

    m

--- Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication-V1/SampleOOPApplication/Program.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication-V1/SampleOOPApplication/Program.cs
@@ -6,10 +6,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("------ Start of Sample Object Oriented Application Made From Scratch -------");
-        
+
         Person aPerson = new Person("John", 78, 250.6, 69, "Male");
         Console.WriteLine(aPerson);
-        
+
+
         Console.WriteLine("------ End of Sample Object Oriented Application Made From Scratch -------");
     } // End of Main()
+
 }
+
+//if (aPerson.Equals(aPerson2)) // This will call the Equals method
+//{
+   // Console.WriteLine("aPerson and aPerson2 are the same object");
+//}
+//else
+//{
+   // Console.WriteLine("aPerson and aPerson2 are different objects");
+//}
